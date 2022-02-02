92dcfcb4303c7757cd3e79a360b2bb1b07b0b697
Krystian Shanahan <shanahan.k@etoncollege.org.uk>
2022-02-02 15:59:03 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Shanahan, Krystian (JD) on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
